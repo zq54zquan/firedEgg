96badba7422aef2deeed22f91f9619c7788209f9
zhouquan <zhouquan02@bilibili.com>
2017-09-30 00:12:00 +0800

    [DATA,SERVER]:output thumb and hd as array in the interface

--- server/lua/ox.lua
+++ server/lua/ox.lua
@@ -1,14 +1,40 @@
 local dbhelp = require "db.db_help"
 local code,db = dbhelp:con_db()
+local function strSplit(delimeter, str)  
+    local find, sub, insert = string.find, string.sub, table.insert  
+    local res = {}  
+    local start, start_pos, end_pos = 1, 1, 1  
+    while true do  
+        start_pos, end_pos = find(str, delimeter, start, true)  
+        if not start_pos then  
+            break  
+        end  
+        insert(res, sub(str, start, start_pos - 1))  
+        start = end_pos + 1    
+    end  
+    insert(res, sub(str,start))  
+    return res  
+end  
+
 local function get_data(page,page_size) 
     if code ~=  dbhelp.DB_CONN_FAILED then
         local st,res = dbhelp:db_query_with_page(db,page,page_size,dbhelp.DB_ORDER_TYPE_SUPPORT)
-		if st == dbhelp.DB_CONN_SUCCESS then 
-			local cjson = require "cjson"
-			ngx.say(cjson.encode({code=200,data=res}));
-		else 
-			ngx.say(cjson.encode({code=200,data=nil}))
+	if st == dbhelp.DB_CONN_SUCCESS then 
+		local cjson = require "cjson"
+		local data = res
+		for v =1,#data do
+		    local hd = res[v]['hd']
+		    hd = strSplit(',',hd)
+		    local img = res[v]['thumb']
+		    img = strSplit(',',img)
+		    res[v]['hd']=hd
+	            res[v]['thumb']=img
 		end
+		local result = cjson.encode({code=200,data=res});
+		ngx.say(result)
+	else 
+		ngx.say(cjson.encode({code=200,data=nil}))
+	end
     else
 		ngx.say(cjson.encode({code=200,data=nil}))
     end
