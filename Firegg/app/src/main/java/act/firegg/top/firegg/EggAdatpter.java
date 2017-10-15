52c0a2a06738bbf0b9dac52483cd2d5bcb7e99ff
zhou quan <zhouquan02@bilibili.com>
2017-10-15 17:48:54 +0800

    [MOBILE]: ADD LOAD MORE AND REFRESH

--- Firegg/app/src/main/java/act/firegg/top/firegg/EggAdatpter.java
+++ Firegg/app/src/main/java/act/firegg/top/firegg/EggAdatpter.java
@@ -1,11 +1,14 @@
 package act.firegg.top.firegg;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -19,6 +22,7 @@ import java.util.List;
 public class EggAdatpter extends RecyclerView.Adapter<EggAdatpter.ViewHolder> {
     private List<EggElemet> eggElemets;
     private Context mContext;
+    private int screenWidth = 0;
     static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
         ImageView eggImage;
@@ -38,6 +42,13 @@ public class EggAdatpter extends RecyclerView.Adapter<EggAdatpter.ViewHolder> {
         if (mContext == null) {
             mContext = parent.getContext();
         }
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE);
+            Point p = new Point();
+            wm.getDefaultDisplay().getSize(p);
+            screenWidth = p.x;
+
+        }
         View view = LayoutInflater.from(mContext).inflate(R.layout.egg_card,parent,false);
         return new ViewHolder(view);
     }
@@ -45,7 +56,17 @@ public class EggAdatpter extends RecyclerView.Adapter<EggAdatpter.ViewHolder> {
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         EggElemet egg = eggElemets.get(position);
-        Glide.with(mContext).load(egg.getThumb()).into(holder.eggImage);
+        ViewGroup.LayoutParams layout = holder.eggImage.getLayoutParams();
+        layout.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        layout.width = screenWidth;
+        holder.eggImage.setLayoutParams(layout);
+        holder.eggImage.setMaxHeight(screenWidth*100);
+        holder.eggImage.setMaxWidth(screenWidth);
+        if (egg.getThumb().get(0).endsWith("gif")) {
+            Glide.with(mContext).load(egg.getThumb().get(0)).asGif().into(holder.eggImage);
+        }else {
+            Glide.with(mContext).load(egg.getThumb().get(0)).into(holder.eggImage);
+        }
     }
 
     @Override
--- Firegg/app/src/main/java/act/firegg/top/firegg/MainActivity.java
+++ Firegg/app/src/main/java/act/firegg/top/firegg/MainActivity.java
@@ -1,5 +1,6 @@
 package act.firegg.top.firegg;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -9,10 +10,16 @@ import android.support.v7.widget.Toolbar;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -20,6 +27,8 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private ArrayList<EggElemet> eggList= new ArrayList<EggElemet>();
     private EggAdatpter adatpter = new EggAdatpter(eggList);
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private int page= 0 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,32 +39,77 @@ public class MainActivity extends AppCompatActivity {
         GridLayoutManager layoutManager = new GridLayoutManager(this,1);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adatpter);
-        this.loadEggs();
+        this.swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.rfr);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                MainActivity.this.loadEggs(0,10);
+            }
+        });
+        recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int currentPage) {
+                MainActivity.this.loadEggs(MainActivity.this.page,10);
+            }
+        });
+        this.loadEggs(0,10);
     }
 
-    private void loadEggs() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder().url("http://www.firegg.top:8099/api/ox")
-                            .build();
-                    Response response = client.newCall(request).execute();
+    private void loadEggs(final int page , int size) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("http://www.firegg.top:8099/api/ox?page="+page+"&size="+size)
+                .build();
+        try {
+                client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            MainActivity.this.swipeRefreshLayout.setRefreshing(false);
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (page == 0 ){
+                        MainActivity.this.eggList.clear();
+                    }
                     String responseData = response.body().string();
                     parseJSON(responseData);
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
-            }
-        }).start();
+            });
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void parseJSON(String str) {
-        Gson gson = new Gson();
-        Type type = new TypeToken<List<EggElemet>>() {}.getType();
-        List<EggElemet> list = gson.fromJson(str,type);
-        eggList.addAll(list);
+        try {
+            JSONObject jsonobj = new JSONObject(str);
+            if (jsonobj.getInt("code") == 200) {
+                Gson gson = new Gson();
+                Type type = new TypeToken<List<EggElemet>>() {
+                }.getType();
+                List<EggElemet> list = gson.fromJson(jsonobj.getJSONArray("data").toString(), type);
+                eggList.addAll(list);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adatpter.notifyDataSetChanged();
+                        MainActivity.this.swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+                ++page;
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
